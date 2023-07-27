7f9fea1ca6b7aed68dd4cad1d909242e0d9ee380
rahulxo19 <rahulchhikara2001@gmail.com>
2023-07-27 10:20:44 +0530

    added refs for uncontrolled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from "react";
 
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) => {
+  const addUserHandler = (uName, uAge, uCollege) => {
     setUsersList((prevUsersList) => {
       return [
         ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
+        {
+          name: uName,
+          age: uAge,
+          college: uCollege,
+          id: Math.random().toString(),
+        },
       ];
     });
   };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
